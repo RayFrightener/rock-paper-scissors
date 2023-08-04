8aa9d075275474f994062eb065437c2305ca0ff5
Pranshu <pranshuchawla19@gmail.com>
2023-08-04 13:22:10 -0400

    function to play 1 game added

--- script.js
+++ script.js
@@ -5,7 +5,43 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+if(playerSelection === computerSelection){
+    let result = "The match is a draw, both parties chose the same option."
+    return result;
+}
+else if(computerSelection === "paper" && playerSelection === "scissor"){
+    let result = "You win! Scissors beat Rock.";
+    return result;
+}
+else if(computerSelection === "paper" && playerSelection === "rock"){
+    let result = "You Lose! Paper beats Rock.";
+    return result;
+}
+ else if(computerSelection === "scissor" && playerSelection === "rock"){
+    let result = "You win! Rock beats Scissor.";
+    return result;
+}
+else if(computerSelection === "scissor" && playerSelection === "paper"){
+    let result = "You lose! Scissor beats Paper.";
+    return result;
+}
+else if(computerSelection === "rock" && playerSelection === "paper"){
+    let result = "You win! Paper beats Rock.";
+    return result;
+}
+else if(computerSelection === "rock" && playerSelection === "scissor"){
+    let result = "You lose! Rock beats Scissor.";
+    return result;
+} else{
+    let result = "Input is wrong, please choose between rock, paper and scissor";
+    return result;
+}
+
+}
 
 
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
 
-console.log(getComputerChoice());
+console.log(playRound(playerSelection, computerSelection));
