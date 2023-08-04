4a434ec9dda15500b626c57953e455996a19470e
Pranshu <pranshuchawla19@gmail.com>
2023-08-04 14:02:39 -0400

    made user input case insensitive and cleaned the code

--- script.js
+++ script.js
@@ -6,42 +6,29 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection) {
-if(playerSelection === computerSelection){
-    let result = "The match is a draw, both parties chose the same option."
-    return result;
-}
-else if(computerSelection === "paper" && playerSelection === "scissor"){
-    let result = "You win! Scissors beat Rock.";
-    return result;
-}
-else if(computerSelection === "paper" && playerSelection === "rock"){
-    let result = "You Lose! Paper beats Rock.";
-    return result;
-}
- else if(computerSelection === "scissor" && playerSelection === "rock"){
-    let result = "You win! Rock beats Scissor.";
-    return result;
-}
-else if(computerSelection === "scissor" && playerSelection === "paper"){
-    let result = "You lose! Scissor beats Paper.";
-    return result;
-}
-else if(computerSelection === "rock" && playerSelection === "paper"){
-    let result = "You win! Paper beats Rock.";
-    return result;
-}
-else if(computerSelection === "rock" && playerSelection === "scissor"){
-    let result = "You lose! Rock beats Scissor.";
-    return result;
-} else{
-    let result = "Input is wrong, please choose between rock, paper and scissor";
-    return result;
+    const lowerCaseInput = playerSelection.toLowerCase();
+if(computerSelection === lowerCaseInput){
+    return "The match is a draw, both parties chose the same option.";
+}else if(computerSelection === "paper" && lowerCaseInput === "scissor"){
+    return "You win! Scissors beat Rock.";
+}else if(computerSelection === "paper" && lowerCaseInput === "rock"){
+    return "You Lose! Paper beats Rock.";
+}else if(computerSelection === "scissor" && lowerCaseInput === "rock"){
+    return "You win! Rock beats Scissor.";
+}else if(computerSelection === "scissor" && lowerCaseInput === "paper"){
+    return "You lose! Scissor beats Paper.";
+}else if(computerSelection === "rock" && lowerCaseInput === "paper"){
+    return "You win! Paper beats Rock.";
+}else if(computerSelection === "rock" && lowerCaseInput === "scissor"){ 
+    return "You lose! Rock beats Scissor.";
+}else{ 
+    return "Input is wrong, please choose between rock, paper and scissor";
 }
 
 }
 
 
-const playerSelection = "paper";
+const playerSelection = "RoCk";
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
