e68d2189ef6f25864d23993c53b0a215a87e8a89
Pranshu <pranshuchawla19@gmail.com>
2023-08-04 21:12:29 -0400

    some issues with git usage this should resolve

--- script.js
+++ script.js
@@ -8,11 +8,7 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection) {
     const lowerCaseInput = playerSelection.toLowerCase();
 if(computerSelection === lowerCaseInput){
-<<<<<<< HEAD
     return "Draw";
-=======
-    return "The match is a draw, both parties chose the same option.";
->>>>>>> 4a434ec9dda15500b626c57953e455996a19470e
 }else if(computerSelection === "paper" && lowerCaseInput === "scissor"){
     return "You win! Scissors beat Rock.";
 }else if(computerSelection === "paper" && lowerCaseInput === "rock"){
@@ -32,11 +28,6 @@ if(computerSelection === lowerCaseInput){
 }
 
 
-<<<<<<< HEAD
-// const playerSelection = "RoCk";
-// const computerSelection = getComputerChoice();
-
-// console.log(playRound(playerSelection, computerSelection));
 function userInput(){
     let input = window.prompt("Choose between Rock, Paper and Scissors: ");
     return input;
@@ -126,9 +117,3 @@ function game(){
 }
 
 game();
-=======
-const playerSelection = "RoCk";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
->>>>>>> 4a434ec9dda15500b626c57953e455996a19470e
